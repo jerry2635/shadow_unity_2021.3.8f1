e659c4e1ee1cdfc77857ba593d2179e685fd2eaa
jerry <jerry731106@gmail.com>
2022-09-18 23:57:34 +0800

    update 0918/02 notyet

--- shadow_unity_2021.3.8f1/Assets/C#/T01.cs
+++ shadow_unity_2021.3.8f1/Assets/C#/T01.cs
@@ -1,3 +1,4 @@
+using System.Runtime.ConstrainedExecution;
 using UnityEngine;
 
 namespace jerry
@@ -9,17 +10,25 @@ namespace jerry
     {
         public float ControlSpeedfloat; //速度控制浮點數
 
-        private void Update()
+        private Transform tank;
+        // 速度
+        private float speed = 20f;
+        // 角速度 
+        private float angle = 60f;
+        public void Update()
         {
-            transform.Translate(Input.GetAxis("Horizontal") * ControlSpeedfloat * Time.deltaTime, Input.GetAxis("Vertical") * ControlSpeedfloat * Time.deltaTime, 0);
-            //transform.position = new Vector3(Mathf.Clamp(transform.position.x, MinX, MaxX), Mathf.Clamp(transform.position.y, MinY, MaxY), transform.position.z);
-            //clamp 限制.藉由產生數值設定介面.避免物件跑出範圍外--固定座標.
+            // 左右偏移(-1,1)
+            float hor = Input.GetAxis("Horizontal");
+            // 前後偏移(-1,1)
+            float ver = Input.GetAxis("Vertical");
+            if (hor != 0 || ver != 0)
+            {
+                // 控制坦克前後行走
+                transform.Translate(Vector3.forward * Time.deltaTime * speed * ver);
+                // 控制坦克左右旋轉
+                transform.Rotate(Vector3.up * Time.deltaTime * angle * hor);
+            }
 
-        }
-
-        public void QuitGame()//遊戲結束
-        {
-            Application.Quit();
         }
     }
 }
