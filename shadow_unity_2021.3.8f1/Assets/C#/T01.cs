03485e5e0a2959cbd7e816b3f91e8b3b7a4956a7
jerry <jerry731106@gmail.com>
2022-09-18 17:04:51 +0800

    up data 0918 athome

--- shadow_unity_2021.3.8f1/Assets/C#/T01.cs
+++ shadow_unity_2021.3.8f1/Assets/C#/T01.cs
@@ -7,13 +7,20 @@ namespace jerry
     /// </summary>
     public class T01 : MonoBehaviour
     {
-        public float ControlSpeed; //浮點數
+        public float ControlSpeedfloat; //速度控制浮點數
 
         private void Update()
         {
-            transform.Translate(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+            transform.Translate(Input.GetAxis("Horizontal") * ControlSpeedfloat * Time.deltaTime, Input.GetAxis("Vertical") * ControlSpeedfloat * Time.deltaTime, 0);
+            //transform.position = new Vector3(Mathf.Clamp(transform.position.x, MinX, MaxX), Mathf.Clamp(transform.position.y, MinY, MaxY), transform.position.z);
+            //clamp 限制.藉由產生數值設定介面.避免物件跑出範圍外--固定座標.
+
         }
 
+        public void QuitGame()//遊戲結束
+        {
+            Application.Quit();
+        }
     }
 }
 
